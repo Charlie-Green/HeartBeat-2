8d3f9ff0b69d3023a21b12e6038ced6a6c9de32e
Charlie-Green <vadim.churun@gmail.com>
2019-12-28 14:53:38 +0300

    Initial commit

---
+++ app/src/main/java/by/vadim_churun/individual/heartbeat2/HeartBeatMainActivity.kt
@@ -0,0 +1,12 @@
+package by.vadim_churun.individual.heartbeat2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class HeartBeatMainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        super.setContentView(R.layout.main_activity)
+    }
+}
